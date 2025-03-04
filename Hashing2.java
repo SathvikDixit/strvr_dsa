1eba73b00970de39b9cf26f1742261f3a2ead7b5
SathvikDixit <sathvikdixit21@gmail.com>
2025-03-04 08:15:49 +0530

    learnt character hashing

--- Hashing2.java
+++ Hashing2.java
@@ -1,7 +1,26 @@
 // Learning Hashing  "THIS IS CHARACTER HASHING"
 
+import java.util.Scanner;
+import java.util.*;
 public class Hashing2 {
     public static void main(String[] args) {
-        
+        String  s;
+        System.out.print("Enter the String: ");
+        Scanner sc = new Scanner(System.in);
+        s = sc.next();
+
+        int[] hash = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            hash[s.charAt(i) - 'a']++;
+        }
+        System.out.print("Enter the number of quaries: ");
+        int q;
+        q = sc.nextInt();
+        System.out.print("Enter the Character the needs to be hashed: ");
+        while (q-- > 0) {
+            char c;
+            c = sc.next().charAt(0);
+            System.out.println(hash[c - 'a']);
+        }
     }
 }
