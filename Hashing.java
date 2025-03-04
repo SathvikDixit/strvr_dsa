a0bda3274afcf3f832939f14eed432a413f7c294
SathvikDixit <sathvikdixit21@gmail.com>
2025-03-04 07:46:53 +0530

    made some changes in the code

--- Hashing.java
+++ Hashing.java
@@ -1,7 +1,32 @@
 // Learning Hashing  "THIS IS INTEGER HASHING"
 
 import java.util.*;
+
 class Hashing {
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.err.print("Enter the number of elements in an array: ");
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter the array elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] hash = new int[13];
+        for (int i = 0; i < n; i++) {
+            hash[arr[i]] += 1;
+        }
+
+        int q;
+        System.err.print("Enter the numner of queries: ");
+        q = sc.nextInt();
+        System.err.print("Enter the number that needs to hash: ");
+        while (q-- != 0) {
+            int number;
+            number = sc.nextInt();
+            System.out.println(hash[number]);
+        }
+    }
 }
