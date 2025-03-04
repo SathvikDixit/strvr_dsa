3a7095c9b7c65c1ef482a436c8a9bf24cf2b2ca1
SathvikDixit <sathvikdixit21@gmail.com>
2025-03-04 22:05:14 +0530

    owiqeurpoiqwer

--- SelectionSort.java
+++ SelectionSort.java
@@ -3,24 +3,5 @@ import javax.sound.midi.SysexMessage;
 
 // Selection Sort
 public class SelectionSort {
-    public static void main (String[] args) {
-        int nums[] = {6,5,2,8,9,4};
-        int size = nums.length;
-        int temp = 0;
-
-        System.err.println("Before sorting: ");
-        for (int num : nums) {
-            System.err.print(num+" ");
-        }
-
-
-
-
-
-        System.out.println();
-        System.out.println("After sorting");
-        for (int num : nums) {
-            System.out.print(num+ " ");
-        }
-    }
+    
 }
