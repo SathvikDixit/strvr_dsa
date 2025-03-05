088c592086d9671e03b7f85defc0ca13e6e75a45
SathvikDixit <sathvikdixit21@gmail.com>
2025-03-05 22:49:27 +0530

    oqweiuroqiwer

--- BinarySearch1.java
+++ BinarySearch1.java
@@ -1,5 +1,18 @@
 // Strivers 1st Binary Search Problem 
 
 public class BinarySearch1 {
+    public static int binarySearch(int[] arr, int target) {
+        int n = arr.length;
+        int low = 0, high = n-1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) return mid;
+            else if (target > arr[mid]) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
+    }
+
     
 }
