6dd6e97d2fade176a3783beec4cde4bfc5b12f32
SathvikDixit <sathvikdixit21@gmail.com>
2025-03-10 19:10:38 +0530

    started solving lower bound problem

--- LowerBound.java
+++ LowerBound.java
@@ -1,5 +1,9 @@
 // This is the 2nd problem in binary search
-
 public class LowerBound {
-    
+    public static int lowerBound(int[] arr, int x, int n) {
+        int low = 0, high = n - 1;
+        int ans = n;
+
+
+
 }
