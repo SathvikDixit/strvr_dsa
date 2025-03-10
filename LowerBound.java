f6be98ec9e9eb4c1cc23d2b5962a64eb6d4ff8a6
SathvikDixit <sathvikdixit21@gmail.com>
2025-03-10 19:11:02 +0530

    Wrote the logic function

--- LowerBound.java
+++ LowerBound.java
@@ -4,6 +4,18 @@ public class LowerBound {
         int low = 0, high = n - 1;
         int ans = n;
 
+        while (low <= high) {
+            int mid = (low + high) / 2;
 
+            if (arr[mid] >= x) {
+                ans = mid;
+                high = mid - 1;
+            } 
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
 
 }
