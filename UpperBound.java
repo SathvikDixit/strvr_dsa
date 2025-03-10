2d49fce1eae42cb7cb79c22cb97613e45ad56318
SathvikDixit <sathvikdixit21@gmail.com>
2025-03-10 20:37:27 +0530

    upper bound

--- UpperBound.java
+++ UpperBound.java
@@ -1,6 +1,29 @@
 // This is the 3rd problem in Binary Search
-
-
 public class UpperBound {
-    
+    public static int upperBound(int arr[], int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high)/2;
+            if (arr[mid] > x) {
+                ans = mid;
+                high = mid -1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+
+
+    public static void main(String[] args) {
+        int arr[] = {3, 5, 8, 9, 15, 19};
+        int n = 6, x = 6;
+        int soln = upperBound(arr, n, x);
+
+        System.out.println("The upper bound of " +x+ " is "+soln);
+    }
 }
