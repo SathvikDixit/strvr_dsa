4284ea40da62542fc3501a3c1329cf5935aa6248
SathvikDixit <sathvikdixit21@gmail.com>
2025-03-12 12:11:38 +0530

    asdfasdfassd

--- SearchInRotatdSortdArr.java
+++ SearchInRotatdSortdArr.java
@@ -7,11 +7,34 @@ public class SearchInRotatdSortdArr {
 
         while (low <= high) {
             int mid = (low + high) / 2;
-            if (arr[mid] == target) return mid;
+            if (arr[mid] == target)
+                return mid;
 
             // Left Sorted
             if (arr[low] <= arr[mid]) {
                 if (arr[low] <= target && target <= arr[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+
+            // Right Sorted
+            else {
+                if (arr[mid] <= target && target <= arr[high])
+                    high = mid - 1;
+                else
+                    high = mid - 1;
+            }
         }
+        return -1;
     }
+
+    public static void main(String[] args) {
+        int arr[] = {7, 8, 9, 1, 2, 3, 4, 5, 6};
+        int n = arr.length, target = 11;
+
+        int result = asdf(arr, n, target);
+        System.err.println(""+result);
+    }
+
 }
