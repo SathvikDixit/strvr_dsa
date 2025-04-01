167eec40f8ee730ca8601b851f54fabb2fa7b352
SathvikDixit <sathvikdixit21@gmail.com>
2025-04-01 22:19:00 +0530

    Gotch youhhh

--- Arrays/LargestInArray.java
+++ Arrays/LargestInArray.java
@@ -1,5 +1,17 @@
-package Arrays;
 
 public class LargestInArray {
-    
+    static int findLargestInArray (int arr[]) {
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 6, 2, 7, 9};
+        System.out.println("Largest number in array is: " + findLargestInArray(array));
+    }
 }
